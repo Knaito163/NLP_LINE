07b2e28bc973911975bc8c4ce7a7c0ca50cc40d7
Knaito163 <deen.sea210@gmail.com>
2016-12-31 00:19:09 +0900

    Add files via upload

--- count_spaceWord.py
+++ count_spaceWord.py
@@ -3,11 +3,16 @@ import glob
 import sys
 import os
 from urllib.request import urlopen
-#import numpy as np
+import numpy as np
 import pandas as pd
 
 
 file_name = sys.argv[1]
+output_file = file_name.rstrip(".txt") + "_countword_result.csv"
+output_fl = output_file.split("/")
+output_file = output_fl[len(output_fl)-1]
+output_folder = sys.argv[2]
+output_path = output_folder + "/" + output_file
 
 # stopワードの除去
 stopword_path = "http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt"
@@ -18,10 +23,10 @@ for line in stopword_file:
   if(stopwords != u""):
     stopwords_list.append(stopwords)
 # 個人的な除去したいストップワード
-private_stopword = ["。", "、",""]
+private_stopword = ["。", "、","て","た","の","に",'"',"か","し","と","ます","ね","は","を","だ","な","が"]
 for ps in private_stopword:
   stopwords_list.append(ps)
-print(stopwords_list)
+# print(stopwords_list)
 
 word_dict = {}
 f = open(file_name, "r")
@@ -31,26 +36,27 @@ for line in f:
     if(w not in stopwords_list):
       if(w not in word_dict):
         word_dict[w] = 1
-        print("in")
+        # print("in")
       elif(w in word_dict):
         word_dict[w] += 1
-        print("not in")
+        # print("not in")
     else:
-      print("Stop")
-print(word_dict)
+      pass
+      # print("Stop")
+# print(word_dict)
 #降順で並び変えて表示
 word_diclist = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
-print(word_diclist)
+# print(word_diclist)
 setkey = []
 setvalue = []
 for wdl in word_diclist:
   #culmun
   setkey.append(wdl[0])
   setvalue.append(wdl[1])
-print(setkey)
-print(setvalue)
+# print(setkey)
+# print(setvalue)
 
 df_columns = ["単語", "出現数"]
 
 df = pd.DataFrame(word_diclist, columns=df_columns)
-df.to_csv("word_results.csv", mode="w")
+df.to_csv(output_path, mode="w")
